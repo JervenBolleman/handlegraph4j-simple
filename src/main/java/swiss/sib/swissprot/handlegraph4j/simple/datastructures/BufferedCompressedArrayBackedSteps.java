5262463872e3bb7b64afde78ed726ee3dcbdd01d
Jerven bolleman <jerven.bolleman@sib.swiss>
2022-03-04 17:01:25 +0100

    Fewer mapped byte buffers to save more ram

--- src/main/java/swiss/sib/swissprot/handlegraph4j/simple/datastructures/BufferedCompressedArrayBackedSteps.java
+++ src/main/java/swiss/sib/swissprot/handlegraph4j/simple/datastructures/BufferedCompressedArrayBackedSteps.java
@@ -38,16 +38,27 @@ public class BufferedCompressedArrayBackedSteps implements Steps {
 	private static class Segment {
 		public Segment(RandomAccessFile raf) throws IOException {
 			super();
-			this.lowLength = raf.readInt();
+			this.lowBitsLength = raf.readInt();
 			this.highBitsLength = raf.readInt();
-			int endOfSegment = (lowLength * Integer.BYTES) + (highBitsLength * Integer.BYTES);
+			int endOfSegment = (lowBitsLength * Integer.BYTES) + (highBitsLength * Integer.BYTES);
 			MappedByteBuffer map = raf.getChannel().map(MapMode.READ_ONLY, raf.getFilePointer(), endOfSegment);
 			this.data = map.asIntBuffer();
 			raf.seek(raf.getFilePointer() + endOfSegment);
 		}
 
+		public Segment(MappedByteBuffer map, int offset) throws IOException {
+			super();
+			this.lowBitsLength = map.getInt(offset);
+			this.highBitsLength = map.getInt(offset + Integer.BYTES);
+			int endOfSegment = (lowBitsLength * Integer.BYTES) + (highBitsLength * Integer.BYTES);
+			int from = offset + (2 * Integer.BYTES);
+			this.data = map.slice(from, endOfSegment).asIntBuffer();
+			assert(this.data.limit() == this.lowBitsLength +this.highBitsLength);
+//			map.position(map.position() + endOfSegment);
+		}
+
 		private final IntBuffer data;
-		private final int lowLength;
+		private final int lowBitsLength;
 		private final int highBitsLength;
 
 		public int length() {
@@ -64,13 +75,13 @@ public class BufferedCompressedArrayBackedSteps implements Steps {
 
 		private int[] uncompressHigh() {
 			int[] highBits = new int[highBitsLength];
-			data.get(lowLength, highBits, 0, highBitsLength);
+			data.get(lowBitsLength, highBits, 0, highBitsLength);
 			return iic.uncompress(highBits);
 		}
 
 		private int[] uncompressLow() {
-			int[] lowBits = new int[lowLength];
-			data.get(0, lowBits, 0, lowLength);
+			int[] lowBits = new int[lowBitsLength];
+			data.get(0, lowBits, 0, lowBitsLength);
 			return iic.uncompress(lowBits);
 		}
 
@@ -85,14 +96,38 @@ public class BufferedCompressedArrayBackedSteps implements Steps {
 	public BufferedCompressedArrayBackedSteps(RandomAccessFile raf) throws IOException {
 		int length = raf.readInt();
 		int i = 0;
+		long start = raf.getFilePointer();
+		int readSegments = 0;
+		List<Integer> segmentStarts = new ArrayList<>();
 		while (i < length - SEGMENT_SIZE) {
-			segments.add(new Segment(raf));
+			segmentStarts.add((int) (raf.getFilePointer() - start));
+			int lowBitsLength = raf.readInt();
+			int highBitsLength = raf.readInt();
+			raf.skipBytes((highBitsLength * Integer.BYTES) + (lowBitsLength * Integer.BYTES));
+			readSegments++;
+			if (raf.getFilePointer() - start > Integer.MAX_VALUE) {
+				openSteps(raf, start, readSegments, segmentStarts);
+				readSegments = 0;
+				start = raf.getFilePointer();
+				segmentStarts.clear();
+			}
 			i += SEGMENT_SIZE;
 		}
+
+		openSteps(raf, start, readSegments, segmentStarts);
+
 		if (i != length) {
 			segments.add(new Segment(raf));
 		}
 		this.length = length;
+	}
+
+	private void openSteps(RandomAccessFile raf, long start, int readSegments, List<Integer> segmentStarts)
+			throws IOException {
+		MappedByteBuffer map = raf.getChannel().map(MapMode.READ_ONLY, start, raf.getFilePointer() - start);
+		for (int j = 0; j < readSegments; j++) {
+			segments.add(new Segment(map, segmentStarts.get(j)));
+		}
 	}
 
 	public static void write(DataOutputStream dos, Steps steps) throws IOException {
